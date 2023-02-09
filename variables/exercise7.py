aa440d40a49fec13308aed7266ad5e5dd09841ff
Baha Özşahin <bahaozsahin@gmail.com>
2023-02-09 17:55:16 +0300

    7 8 9

---
+++ variables/exercise7.py
@@ -0,0 +1,10 @@
+multi_line_string = """
+Hello,
+It is a
+multiple line
+string
+"""
+
+string = "a normal string with double quotes"
+
+string2 = 'another normal string with single quotes'
---
+++ variables/exercise9.py
@@ -0,0 +1,6 @@
+a = 32
+b = 'string'
+temp = a
+a = b
+b = temp
+print (a,b)
